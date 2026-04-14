0a8d55637026a95627300c26a1db3da452c1186f
agudeloalison74-netizen <agudeloalison74@gmail.com>
2026-04-14 18:38:41 -0500

    funcion de mostrar aprendices

---
+++ app.js
@@ -0,0 +1,12 @@
+const aprendices = [
+    {nombre: "Ana", nota: 4.5, programa: "ADSO"},
+    {nombre: "Luis", nota: 3.2, programa: "ADSO"},
+    {nombre: "Maria", nota: 4.8, programa: "ADSO"},
+    {nombre: "Carlos", nota: 2.9, programa: "ADSO"},
+    {nombre: "Sofia", nota: 4.0, programa: "ADSO"}
+];
+
+function mostrarAprendices() {
+    console.log("Todos los aprendices:");
+    console.table(aprendices);
+}
