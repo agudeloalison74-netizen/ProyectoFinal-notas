aa2023ae339a211cd91747e0b247f17d40d741d3
agudeloalison74-netizen <agudeloalison74@gmail.com>
2026-04-14 20:29:13 -0500

    funcion menu

--- app.js
+++ app.js
@@ -6,3 +6,56 @@ const aprendices = [
     {nombre: "Sofia", nota: 4.0, programa: "ADSO"}
 ];
 
+function menu() {
+    let opcion = 0;
+
+    while (opcion !== 8) {
+     opcion = Number(prompt(`
+        1. ver aprendices
+        2: Aprobados
+        3: Reprobados
+        4. Mayúsculas
+        5. Promedio
+        6. Ordenar
+        7. Clasificar nota
+        8. Salir
+     `));
+
+        switch (opcion) {
+            case 1:
+                mostrarAprendices();
+                break;
+
+            case 2:
+                aprobados();
+                break;
+
+            case 3:
+                reprobados();
+                break;
+                
+            case 4:
+                nombresmayus();
+                break;
+
+            case 5:
+                promedio();
+                break;
+
+            case 6:
+                ordenar();
+                break;
+
+            case 7:
+                calificarNota();
+                break;
+
+            case 8:
+                alert("¡Hasta luego!");
+                break;
+                
+            default:
+                alert("Opción no válida, por favor intente de nuevo.");
+        }   
+    }
+}
