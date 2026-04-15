fae9338dbe2612901f5e13998c394e1bdb46c3e0
1902yayitabc-collab <1902yayitabc@gmail.com>
2026-04-14 20:03:22 -0500

    nota

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const aprendices = [
     {nombre: "Sofia", nota: 4.0, programa: "ADSO"}
 ];
 
+function calificarNota(){
+    let nota = Number(prompt("ingresar una nota" ));
+    switch (true){
+        case nota <3.0:
+            console.log("nivel: bajo ");
+            break;
+        case notas < 4.0:
+            console.log("nivel: basico");
+            break;
+        case nota <4.5:
+            console.log("nivel: alto");
+            break;
+        default:
+            console.log("nivel: superior");
+        }
+    }
